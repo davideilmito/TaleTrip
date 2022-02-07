2f153f9b3b928f27549e436a97124de4622f3e9f
willymmerlet12 <willymmerlet@gmail.com>
2022-02-07 15:54:23 +0100

    Initial Commit

---
+++ Taletrip/TaletripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaletripApp.swift
+//  Taletrip
+//
+//  Created by Willy Merlet on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TaletripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
