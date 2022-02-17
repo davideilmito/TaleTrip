c5ebc0503fe938d920c691b1e4c6862f2230c68b
willymmerlet12 <willymmerlet@gmail.com>
2022-02-17 10:29:28 +0100

    story glow

--- SpeechRecognizer.swift
+++ SpeechRecognizer.swift
@@ -4,156 +4,142 @@
 //
 //  Created by Willy Merlet on 14/02/22.
 //
-import AVFoundation
-import Foundation
-import Speech
 import SwiftUI
+import Speech
 
-class SpeechRecognizer: ObservableObject {
-    enum RecognizerError: Error {
-        case nilRecognizer
-        case notAuthorizedToRecognize
-        case notPermittedToRecord
-        case recognizerIsUnavailable
-        
-        var message: String {
-            switch self {
-            case .nilRecognizer: return "Can't initialize speech recognizer"
-            case .notAuthorizedToRecognize: return "Not authorized to recognize speech"
-            case .notPermittedToRecord: return "Not permitted to record audio"
-            case .recognizerIsUnavailable: return "Recognizer is unavailable"
-            }
-        }
-    }
+final class SpeechAnalyzer: NSObject, ObservableObject, SFSpeechRecognizerDelegate {
+    private let audioEngine = AVAudioEngine()
+    private var inputNode: AVAudioInputNode?
+    private var speechRecognizer: SFSpeechRecognizer?
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
     
-    var transcript: String = ""
-    
-    private var audioEngine: AVAudioEngine?
-    private var request: SFSpeechAudioBufferRecognitionRequest?
-    private var task: SFSpeechRecognitionTask?
-    private let recognizer: SFSpeechRecognizer?
-    
-    init() {
-        recognizer = SFSpeechRecognizer()
-        
-        Task(priority: .background) {
-            do {
-                guard recognizer != nil else {
-                    throw RecognizerError.nilRecognizer
-                }
-                guard await SFSpeechRecognizer.hasAuthorizationToRecognize() else {
-                    throw RecognizerError.notAuthorizedToRecognize
-                }
-                guard await AVAudioSession.sharedInstance().hasPermissionToRecord() else {
-                    throw RecognizerError.notPermittedToRecord
-                }
-            } catch {
-                speakError(error)
-            }
-        }
-    }
-    
-    deinit {
-        reset()
-    }
-    
-    func transcribe() {
-        DispatchQueue(label: "Speech Recognizer Queue", qos: .background).async { [weak self] in
-            guard let self = self, let recognizer = self.recognizer, recognizer.isAvailable else {
-                self?.speakError(RecognizerError.recognizerIsUnavailable)
-                return
-            }
-            
-            do {
-                let (audioEngine, request) = try Self.prepareEngine()
-                self.audioEngine = audioEngine
-                self.request = request
-                self.task = recognizer.recognitionTask(with: request, resultHandler: self.recognitionHandler(result:error:))
-            } catch {
-                self.reset()
-                self.speakError(error)
-            }
-        }
-    }
-    
-    func stopTranscribing() {
-        reset()
-    }
-    
-    func reset() {
-        task?.cancel()
-        audioEngine?.stop()
-        audioEngine = nil
-        request = nil
-        task = nil
-    }
-    
-    private static func prepareEngine() throws -> (AVAudioEngine, SFSpeechAudioBufferRecognitionRequest) {
-        let audioEngine = AVAudioEngine()
-        
-        let request = SFSpeechAudioBufferRecognitionRequest()
-        request.shouldReportPartialResults = true
-        
+    @Published var recognizedText: String?
+    @Published var isProcessing: Bool = false
+
+    func start() {
         let audioSession = AVAudioSession.sharedInstance()
-        try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
-        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-        let inputNode = audioEngine.inputNode
+        do {
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("Couldn't configure the audio session properly")
+        }
+        
+        inputNode = audioEngine.inputNode
+        
+        speechRecognizer = SFSpeechRecognizer()
+        print("Supports on device recognition: \(speechRecognizer?.supportsOnDeviceRecognition == true ? "✅" : "🔴")")
+
+        // Force specified locale
+        // self.speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "pl_PL"))
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        // Disable partial results
+        // recognitionRequest?.shouldReportPartialResults = false
+        
+        // Enable on-device recognition
+        // recognitionRequest?.requiresOnDeviceRecognition = true
+
+        guard let speechRecognizer = speechRecognizer,
+              speechRecognizer.isAvailable,
+              let recognitionRequest = recognitionRequest,
+              let inputNode = inputNode
+        else {
+            assertionFailure("Unable to start the speech recognition!")
+            return
+        }
+        
+        speechRecognizer.delegate = self
         
         let recordingFormat = inputNode.outputFormat(forBus: 0)
         inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
-            request.append(buffer)
+            recognitionRequest.append(buffer)
         }
+
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { [weak self] result, error in
+            self?.recognizedText = result?.bestTranscription.formattedString
+            
+            guard error != nil || result?.isFinal == true else { return }
+            self?.stop()
+        }
+
         audioEngine.prepare()
-        try audioEngine.start()
         
-        return (audioEngine, request)
-    }
-    
-    private func recognitionHandler(result: SFSpeechRecognitionResult?, error: Error?) {
-        let receivedFinalResult = result?.isFinal ?? false
-        let receivedError = error != nil
-        
-        if receivedFinalResult || receivedError {
-            audioEngine?.stop()
-            audioEngine?.inputNode.removeTap(onBus: 0)
-        }
-        
-        if let result = result {
-            speak(result.bestTranscription.formattedString)
+        do {
+            try audioEngine.start()
+            isProcessing = true
+        } catch {
+            print("Coudn't start audio engine!")
+            stop()
         }
     }
     
-    private func speak(_ message: String) {
-        transcript = message
+    func stop() {
+        recognitionTask?.cancel()
+        
+        self.audioEngine.stop()
+        inputNode?.removeTap(onBus: 0)
+        
+        isProcessing = false
+        
+        recognitionRequest = nil
+        recognitionTask = nil
+        speechRecognizer = nil
+        inputNode = nil
     }
     
-    private func speakError(_ error: Error) {
-        var errorMessage = ""
-        if let error = error as? RecognizerError {
-            errorMessage += error.message
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            print("✅ Available")
         } else {
-            errorMessage += error.localizedDescription
-        }
-        transcript = "<< \(errorMessage) >>"
-    }
-}
-
-extension SFSpeechRecognizer {
-    static func hasAuthorizationToRecognize() async -> Bool {
-        await withCheckedContinuation { continuation in
-            requestAuthorization { status in
-                continuation.resume(returning: status == .authorized)
-            }
+            print("🔴 Unavailable")
+            recognizedText = "Text recognition unavailable. Sorry!"
+            stop()
         }
     }
 }
 
-extension AVAudioSession {
-    func hasPermissionToRecord() async -> Bool {
-        await withCheckedContinuation { continuation in
-            requestRecordPermission { authorized in
-                continuation.resume(returning: authorized)
+struct SpeechRecognitionView: View {
+    private enum Constans {
+        static let recognizeButtonSide: CGFloat = 100
+    }
+    
+    @ObservedObject private var speechAnalyzer = SpeechAnalyzer()
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(speechAnalyzer.recognizedText ?? "Tap to begin")
+                .padding()
+            
+            Button {
+                toggleSpeechRecognition()
+            } label: {
+                Image(systemName: speechAnalyzer.isProcessing ? "waveform.circle.fill" : "waveform.circle")
+                    .resizable()
+                    .frame(width: Constans.recognizeButtonSide,
+                           height: Constans.recognizeButtonSide,
+                           alignment: .center)
+                    .foregroundColor(speechAnalyzer.isProcessing ? .red : .gray)
+                    .aspectRatio(contentMode: .fit)
             }
+            .padding()
         }
+    }
+}
+
+private extension SpeechRecognitionView {
+    func toggleSpeechRecognition() {
+        if speechAnalyzer.isProcessing {
+            speechAnalyzer.stop()
+        } else {
+            speechAnalyzer.start()
+        }
+    }
+}
+
+struct SpeechRecognitionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeechRecognitionView()
     }
 }
--- Taletrip/HomeView.swift
+++ Taletrip/HomeView.swift
@@ -8,7 +8,7 @@ import SwiftUI
 import AVFoundation
 import SwiftSpeech
 
-struct ContentView: View {
+struct HomeView: View {
     
     var sessionConfiguration: SwiftSpeech.Session.Configuration
     
@@ -42,6 +42,20 @@ struct ContentView: View {
         let synthesizer = AVSpeechSynthesizer()
         synthesizer.speak(speechUtterance)
 }
+    func text2Speeech() {
+    do {
+        try AVAudioSession.sharedInstance().setCategory(.playback,mode: .default)
+        
+    } catch let error {
+        print("\(error.localizedDescription)")
+    }
+    let speechUtterance: AVSpeechUtterance = AVSpeechUtterance(string: "\(intro)")
+    speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate / 2.0
+    speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+    
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speechUtterance)
+}
     enum Fsm {
         case wait, response, check
     }
@@ -49,14 +63,15 @@ struct ContentView: View {
     @State var state: Fsm = .wait
     @State var index: Int = 0
     @State var die: Int = 0
+    @State var list: [(session: SwiftSpeech.Session, text: String)] = []
     
     let desc = [
-        /*0*/"You are working as private investigators in Cramwood. After you got paid for a very difficult case solved, you wanted to take some days off work. You had the idea to go together to a bar, but living in such a small city, there are very few options when it comes to it.\n\nLet’s try to make a choice. This is the first one of many that you are going to do in this adventure. Would you like to go to “Puzzles” or “Beer Garden”?\n",
-        /*1*/"Perfect. Some choices are more difficult than others and will require the roll of dice. You don’t truly remember how to get to the bar, but fortunately you have a map. Try using the map.",
-        /*2*/"You get lost a couple of times, but in the end you arrive at your destination.",
-        /*3*/"With the help of the map you easily arrive at your destination."
+        /*0*/"Perfect. Some choices are more difficult than others and will require the roll of dice. You don’t truly remember how to get to the bar, but fortunately you have a map. Try using the map.",
+        /*1*/"You get lost a couple of times, but in the end you arrive at your destination.",
+        /*2*/"With the help of the map you easily arrive at your destination."
     ]
-    let answer = "Go to beer Garden"
+    let intro = "You are working as private investigators in Cramwood. After you got paid for a very difficult case solved, you wanted to take some days off work. You had the idea to go together to a bar, but living in such a small city, there are very few options when it comes to it.\n\nLet’s try to make a choice. This is the first one of many that you are going to do in this adventure. Would you like to go to “Puzzles” or “Beer Garden”?\n"
+    let answer = "Go to beer garden"
     let answer2 = "Use map"
     
     func correct() {
@@ -85,63 +100,86 @@ struct ContentView: View {
                     Text("The Detective's Day Off")
                         .fontWeight(.bold)
                     Text("Chapter 1")
-                }.frame(width: 200).padding(.trailing, 35)
+                }
+                .frame(width: 200)
+                .padding(.trailing, 35)
                 Image(systemName: "xmark.circle.fill")
                     .font(.title)
               
-            }.frame(width: UIScreen.screenWidth - 20).padding(.top, 20)
+            }
+            .frame(width: UIScreen.screenWidth - 20)
+            .padding(.top, 20)
             ScrollView{
-                Text(desc[index])
+                   Text(intro)
                     .padding()
                     .padding(.trailing, 15)
                     .lineSpacing(6)
                     .onAppear(perform: {
-                        text2Speech()
+                        text2Speeech()
                     })
-                HStack{
+                
+                
                     Spacer()
                     
-                    Text("\(text)")
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("bluish"))
-                        .autocapitalization(.none)
-                        .frame(width: 150)
-                        .padding(.trailing, 35)
-                        .padding(.bottom, 20)
+                    ForEach(list, id: \.session.id) { pair in
+                        VStack(alignment: .trailing){
+                        Text(pair.text)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("bluish"))
+                            .autocapitalization(.none)
+                            .frame(width: 150)
+                            .padding(.trailing, 35)
+                            .padding(.bottom, 20)
+                        }.frame(width: 100)
+                        ForEach(desc, id: \.self) { chunk in
+                         Group {
+                                if (pair.text == answer) {
+                                    Text(chunk)
+                                        .padding()
+                                        .padding(.trailing, 15)
+                                        .lineSpacing(6)
+                                        .onAppear(perform: {
+                                         correct()
+                                        })
+                                }
+                            }
+                            
+                         }
                 }
             
-           
            
                 switch index {
                 case 0:
                     VStack{
-                        if (text == answer) {
-                           Text("")
-                                .onAppear{
-                                    correct()
-                                   
-                                }
-                        } else {
-                            Text("")
-                                .onAppear{
-                                    wrong()
-                                    
-                                }
-                        }
+//                        if (text == answer) {
+//                           Text("")
+//                                .onAppear{
+//                                    correct()
+//
+//                                }
+//                        } else {
+//                            Text("")
+//                                .onAppear{
+//                                    wrong()
+//
+//                                }
+//                        }
+                        Text("")
                     }
                 case 1:
                     VStack{
-                        if(text == answer2) {
-                            Text("")
-                                .onAppear{
-                                    correct()
-                                }
-                        } else {
-                            Text("")
-                                .onAppear{
-                                    wrong()
-                                }
-                        }
+//                        if(text == answer2) {
+//                            Text("")
+//                                .onAppear{
+//                                    correct()
+//                                }
+//                        } else {
+//                            Text("")
+//                                .onAppear{
+//                                    wrong()
+//                                }
+//                        }
+                        Text("")
                     }
                    
                     
@@ -168,7 +206,6 @@ struct ContentView: View {
                     //                else if (state == .response) {
                     //                    //voice
 
-
                     //                    //when it stops
                     //                    state = .check
                     //                }
@@ -179,20 +216,36 @@ struct ContentView: View {
                 }
             Spacer()
                 SwiftSpeech.RecordButton()
-                    .swiftSpeechToggleRecordingOnTap(sessionConfiguration: sessionConfiguration, animation: .spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0))
-                    .onRecognizeLatest(update: $text)
+                    .swiftSpeechToggleRecordingOnTap(sessionConfiguration: sessionConfiguration, animation: .spring(response: 0.4, dampingFraction: 0.4, blendDuration: 0))
+                    .onStartRecording { session in
+                        list.append((session, ""))
+                    }
+                    .onRecognize(includePartialResults: true) { session, result in
+                        list.firstIndex { $0.session.id == session.id }
+                            .map { index in
+                                list[index].text = result.bestTranscription.formattedString + (result.isFinal ? "" : "...")
+                            }
+                    } handleError: { session, error in
+                        list.firstIndex { $0.session.id == session.id }
+                            .map { index in
+                                list[index].text = "Error \((error as NSError).code)"
+                            }
+                    }
+         }
         }
-        }.padding(.top, 35).frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight).background(Color("beige")).onAppear {
+        .padding(.top, 35)
+        .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
+        .background(Color("beige"))
+        .onAppear
+                    {
                 SwiftSpeech.requestSpeechRecognitionAuthorization()
-            }
-            
-           
+                    }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
 
@@ -202,3 +255,6 @@ extension UIScreen{
    static let screenHeight = UIScreen.main.bounds.size.height
    static let screenSize = UIScreen.main.bounds.size
 }
+
+
+
