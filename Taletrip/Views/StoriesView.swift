2aa4be3f3b5fdfaab531e80596c86f173e70843e
willymmerlet12 <willymmerlet@gmail.com>
2022-02-22 10:52:50 +0100

    mortal portrait json

--- Taletrip/Views/StoriesView.swift
+++ Taletrip/Views/StoriesView.swift
@@ -9,53 +9,78 @@ import SwiftUI
 
 struct StoriesView: View {
     
+    init() {
+        UITableView.appearance().separatorStyle = .singleLine
+       }
+
     @StateObject var storiesStore = StoriesStore()
     
     var body: some View {
-        
-        ScrollView(showsIndicators: false){
-            
-            VStack(alignment: .leading, spacing: 35){
+
+        NavigationView{
+            ScrollView(showsIndicators: false){
                 
-                
-                //             Story Of the  Month
-                TextView(title: "Stories",size: 37,weight: .bold)
-                
-                if let storyOfTheMonth = storiesStore.storyOfTheMonth{
+                VStack(alignment: .leading, spacing: 35){
                     
-                    HighlightedCardView(storyToBeHighlighted: storyOfTheMonth,title: "Story Of The Month")
                     
-                }
-                
-            
-            
-                
-                
-                //             All Adventures
-   
-                HorizontalCardsView(stories: storiesStore.adventureStories,title: "Adventure")
-                
-                
-                
-                
-                //             Story You will Like
-                if let storyYouWillLike = storiesStore.storyYouWillLike{
+                    //             Story Of the  Month
+                    TextView(title: "Stories",size: 37,weight: .bold)
                     
-                    HighlightedCardView(storyToBeHighlighted: storyYouWillLike,title: "Story You'll Like")
+                    if let storyOfTheMonth = storiesStore.storyOfTheMonth{
+                        
+                        HighlightedCardView(storyToBeHighlighted: storyOfTheMonth,title: "Story Of The Month")
+                        
+                    }
                     
-                }
                 
-            
-        }.frame(maxWidth:.infinity,alignment: .leading)
-            .padding(.leading,40)
-            
-            
+                
+                    
+                    
+                    //             All Adventures
+       
+                    HorizontalCardsView(stories: storiesStore.adventureStories,title: "Adventure")
+                    
+                    
+                    
+                    
+                    //             Story You will Like
+                    if let storyYouWillLike = storiesStore.storyYouWillLike{
+                        
+                        HighlightedCardView(storyToBeHighlighted: storyYouWillLike,title: "Story You'll Like")
+                        
+                    }
+                    
+                
+            }.frame(maxWidth:.infinity,alignment: .leading)
+                    .padding(.leading,40)
+                    .navigationBarHidden(true)
+                    
+                   
+                
+                
+            }
         }
+        
     }
 }
 
 struct StoriesView_Previews: PreviewProvider {
     static var previews: some View {
         StoriesView()
+    }
+}
+
+
+struct HiddenNavigationBar: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .navigationBarTitle("", displayMode: .inline)
+        .navigationBarHidden(true)
+    }
+}
+
+extension View {
+    func hiddenNavigationBarStyle() -> some View {
+        modifier( HiddenNavigationBar() )
     }
 }
