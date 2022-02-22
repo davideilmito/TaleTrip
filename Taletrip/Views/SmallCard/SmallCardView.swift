30e87524703119ab4d89a73d055d22accfb33aa7
suyashlunawat <92525202+suyashlunawat@users.noreply.github.com>
2022-02-22 11:53:58 +0100

    Capsule View refined for dynamic

--- Taletrip/Views/SmallCard/SmallCardView.swift
+++ Taletrip/Views/SmallCard/SmallCardView.swift
@@ -11,12 +11,18 @@ struct SmallCardView: View {
     
     let storyImageName: String
     let storyTitle : String
+    let length: Longevity
     
     var body: some View {
     
         VStack(alignment: .leading,spacing: 5){
             
-            SmallCardImage(storyImageName: storyImageName)
+            SmallCardImage(storyImageName: storyImageName).overlay {
+                CapsuleView(longevity: length, width:40, height: 14, textSize: 7)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    .padding(.leading, 10)
+                    .padding(.top, 10)
+            }
             
             TextView(title: storyTitle, size: 14, weight: .regular)
                 .foregroundColor(Color.smallCardTitleColor).frame(maxWidth: 149,maxHeight:18,alignment: .leading)
@@ -30,6 +36,6 @@ struct SmallCardView: View {
 
 struct SmallCardView_Previews: PreviewProvider {
     static var previews: some View {
-        SmallCardView(storyImageName: "themortalportrait", storyTitle: "Title 1")
+        SmallCardView(storyImageName: "themortalportrait", storyTitle: "Title 1",length: .long)
     }
 }
